0b1d48c8854fbbabed99b87d2c9bfaf1cdb89e2d
celeste0423 <97865720+celeste0423@users.noreply.github.com>
2023-03-20 15:31:06 +0900

    first Beta

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'page/app.dart';
+import 'firebase_options.dart';
+
+void main() async{ //파이어 베이스 시작하려면 메인 화면에 이거 붙여줄 것
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Diary_clone',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: App(),
+    );
+  }
+}
+
